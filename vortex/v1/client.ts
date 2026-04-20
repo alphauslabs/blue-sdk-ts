9c4a5053a210c338a3fc187ca5547f10571e1b97
Ninolito <ninolitopeligrino@gmail.com>
2026-04-20 09:01:32 +0800

    fix: rename NewAdminClient to NewPrismClient and NewVortexClient for consistency

--- vortex/v1/client.ts
+++ vortex/v1/client.ts
@@ -1,7 +1,7 @@
 import { Vortex } from "./vortex_pb";
 import { BlueAPIService, ConnectOption } from "../../conn/conn";
 
-export function NewAdminClient(opt?: ConnectOption) {
+export function NewVortexClient(opt?: ConnectOption) {
   const blueapi = new BlueAPIService(Vortex, "vortex", opt);
   return blueapi.clientInstance;
 }
