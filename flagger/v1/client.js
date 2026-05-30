dc5d0559eba4b1220281159d2bc43f3af3af94a7
Ninolito <ninolitopeligrino@gmail.com>
2026-05-30 21:58:37 +0800

    fix: update service name in NewFlaggerClient and NewFlagsClient to 'blue'

--- flagger/v1/client.js
+++ flagger/v1/client.js
@@ -4,6 +4,6 @@ exports.NewFlaggerClient = NewFlaggerClient;
 const flagger_pb_1 = require("./flagger_pb");
 const conn_1 = require("../../conn/conn");
 function NewFlaggerClient(opt) {
-    const blueapi = new conn_1.BlueAPIService(flagger_pb_1.Flagger, "flagger", opt);
+    const blueapi = new conn_1.BlueAPIService(flagger_pb_1.Flagger, "blue", opt);
     return blueapi.clientInstance;
 }
--- flags/v1/client.js
+++ flags/v1/client.js
@@ -4,6 +4,6 @@ exports.NewFlagsClient = NewFlagsClient;
 const flags_pb_1 = require("./flags_pb");
 const conn_1 = require("../../conn/conn");
 function NewFlagsClient(opt) {
-    const blueapi = new conn_1.BlueAPIService(flags_pb_1.Flags, "flags", opt);
+    const blueapi = new conn_1.BlueAPIService(flags_pb_1.Flags, "blue", opt);
     return blueapi.clientInstance;
 }
