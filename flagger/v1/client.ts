09507e32335f907aad6b6126881e4db46c27de48
Ninolito <ninolitopeligrino@gmail.com>
2026-05-30 21:27:01 +0800

    fix: update service name in NewFlaggerClient and NewFlagsClient to 'blue'

--- flagger/v1/client.ts
+++ flagger/v1/client.ts
@@ -2,6 +2,6 @@ import { Flagger } from "./flagger_pb";
 import { BlueAPIService, ConnectOption } from "../../conn/conn";
 
 export function NewFlaggerClient(opt?: ConnectOption) {
-  const blueapi = new BlueAPIService(Flagger, "flagger", opt);
+  const blueapi = new BlueAPIService(Flagger, "blue", opt);
   return blueapi.clientInstance;
 }
--- flags/v1/client.ts
+++ flags/v1/client.ts
@@ -2,6 +2,6 @@ import { Flags } from "./flags_pb";
 import { BlueAPIService, ConnectOption } from "../../conn/conn";
 
 export function NewFlagsClient(opt?: ConnectOption) {
-  const blueapi = new BlueAPIService(Flags, "flags", opt);
+  const blueapi = new BlueAPIService(Flags, "blue", opt);
   return blueapi.clientInstance;
 }
